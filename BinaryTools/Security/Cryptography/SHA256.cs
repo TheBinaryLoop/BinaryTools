b62b3fae3a0b0b387ff9759c0a8d1e5781b33351
Lukas Eßmann <l.essmann@directonline.de>
2018-10-10 09:14:28 +0200

    Added the BinaryTools.Security.Cryptography namespace Added class SHA256 Added method SHA256.GetSHA256Hash(string)

---
+++ BinaryTools/Security/Cryptography/SHA256.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace BinaryTools.Security.Cryptography {
+   public static class SHA256 {
+
+      /// <summary>
+      /// Computes the hash value for the specified string. 
+      /// </summary>
+      /// <param name="input">The input to compute the hash code for.</param>
+      /// <returns>Returns the calculated hash value in the form of a hexadecimal string.</returns>
+      public static string GetSHA256Hash(string input) {
+         SHA256Managed module = new SHA256Managed();
+         byte[] output = module.ComputeHash(Encoding.UTF8.GetBytes(input));
+         return output.Aggregate(new StringBuilder(output.Length * 2), (sb, b) => sb.AppendFormat("{0:X2}", b)).ToString();
+      }
+
+   }
+}
