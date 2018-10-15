4c427fd2121bb4c235fa63043a6673b031394b74
Lukas Eßmann <l.essmann@directonline.de>
2018-10-15 15:36:20 +0200

    Added Unit Test Project and first test

---
+++ BinaryTools.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BinaryTools.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BinaryTools.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("be706fca-10fc-4ccd-8875-9dc82a0018d7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ BinaryTools.Tests/RIPEMD160Test.cs
@@ -0,0 +1,13 @@
+﻿using BinaryTools.Security.Cryptography;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BinaryTools.Tests {
+   [TestClass]
+   public class RIPEMD160Test {
+      [TestMethod]
+      public void TestGetRIPEMD160Hash() {
+         string testString = "This is the test string";
+         Assert.AreEqual(RIPEMD160.GetRIPEMD160Hash(testString), "261733CAE4EBD91AE9603C0804F0BF6F96EDCF0A");
+      }
+   }
+}
