5a461b7ae619aecf36c2c5a42e629eca9e183311
Lukas Eßmann <l.essmann@directonline.de>
2018-10-15 15:18:11 +0200

    Added method GetRIPEMD160Hash to the RIPEMD160 class

--- BinaryTools/Security/Cryptography/RIPEMD160.cs
+++ BinaryTools/Security/Cryptography/RIPEMD160.cs
@@ -4,5 +4,16 @@ using System.Text;
 
 namespace BinaryTools.Security.Cryptography {
    public static class RIPEMD160 {
+
+      /// <summary>
+      /// Computes the hash value for the specified string. 
+      /// </summary>
+      /// <param name="input">The input to compute the hash code for.</param>
+      /// <returns>Returns the calculated hash value in the form of a hexadecimal string.</returns>
+      public static string GetRIPEMD160Hash(string input) {
+         RIPEMD160Managed module = new RIPEMD160Managed();
+         byte[] output = module.ComputeHash(Encoding.UTF8.GetBytes(input));
+         return output.Aggregate(new StringBuilder(output.Length * 2), (sb, b) => sb.AppendFormat("{0:X2}", b)).ToString();
+      }
    }
 }
