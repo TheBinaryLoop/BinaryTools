48e77e85c9b2b019f8c54dccb8bed20c44382d63
Lukas Eßmann <l.essmann@directonline.de>
2018-10-15 14:43:05 +0200

    Added extension method ToUnsecureString for the SecureString type

--- BinaryTools/Extensions/SecureStringExtensions.cs
+++ BinaryTools/Extensions/SecureStringExtensions.cs
@@ -5,5 +5,16 @@ using System.Security;
 namespace BinaryTools.Extensions {
    public static class SecureStringExtensions {
 
+      public static string ToUnsecureString(this SecureString secStr) {
+         IntPtr unmanagedString = IntPtr.Zero;
+         try {
+            unmanagedString = Marshal.SecureStringToGlobalAllocUnicode(secStr);
+            return Marshal.PtrToStringUni(unmanagedString);
+         }
+         finally {
+            Marshal.ZeroFreeGlobalAllocUnicode(unmanagedString);
+         }
+      }
+
    }
 }
