4a68558679c5cd93ce569f7d7aa15b92a398563d
Lukas Eßmann <l.essmann@directonline.de>
2018-10-15 14:36:50 +0200

    Added method ToSecureString inside BinaryTools.Extensions.StringExtensions

--- BinaryTools/Extensions/StringExtensions.cs
+++ BinaryTools/Extensions/StringExtensions.cs
@@ -1,10 +1,20 @@
-﻿using System;
+﻿using System.Security;
 
 namespace BinaryTools.Extensions {
    public static class StringExtensions {
 
-      public static string GetSHA256Hash(this String str) {
+      public static string GetSHA256Hash(this string str) {
          return Security.Cryptography.SHA256.GetSHA256Hash(str);
+      }
+
+      public static SecureString ToSecureString(this string str) {
+         SecureString secureString = new SecureString();
+         if (str.Length > 0) {
+            foreach (char c in str.ToCharArray()) {
+               secureString.AppendChar(c);
+            }
+         }
+         return secureString;
       }
    }
 }
