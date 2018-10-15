160031faa66cc3f600b22909039733a9442fe31d
Lukas Eßmann <l.essmann@directonline.de>
2018-10-15 14:31:01 +0200

    Added the BinaryTools.Extensions namespace Added BinaryTools.Extensions.StringExtensions class Added method GetSHA256Hash inside BinaryTools.Extensions.StringExtensions

---
+++ BinaryTools/Extensions/StringExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace BinaryTools.Extensions {
+   public static class StringExtensions {
+
+      public static string GetSHA256Hash(this String str) {
+         return Security.Cryptography.SHA256.GetSHA256Hash(str);
+      }
+   }
+}
