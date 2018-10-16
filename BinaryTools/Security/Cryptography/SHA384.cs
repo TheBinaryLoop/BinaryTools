01054700fad9d6ea8c165df8fafc0cf704d31f31
Lukas Eßmann <l.essmann@directonline.de>
2018-10-16 07:37:49 +0200

    Addedd method ComputeHash(string) to the SHA384 class

--- BinaryTools/Security/Cryptography/SHA384.cs
+++ BinaryTools/Security/Cryptography/SHA384.cs
@@ -1,10 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace BinaryTools.Security.Cryptography {
    public static class SHA384 {
+
+      /// <summary>
+      /// Computes the hash value for the specified string. 
+      /// </summary>
+      /// <param name="input">The input to compute the hash code for.</param>
+      /// <returns>Returns the calculated hash value in the form of a hexadecimal string.</returns>
+      public static string ComputeHash(string input) {
+         SHA384Managed module = new SHA384Managed();
+         byte[] output = module.ComputeHash(Encoding.UTF8.GetBytes(input));
+         return output.Aggregate(new StringBuilder(output.Length * 2), (sb, b) => sb.AppendFormat("{0:X2}", b)).ToString();
+      }
    }
 }
