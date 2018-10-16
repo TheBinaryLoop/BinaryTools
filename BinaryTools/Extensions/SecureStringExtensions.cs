3f5e0cd5cec3d3355346c68cdff57e304631378b
Lukas Eßmann <l.essmann@directonline.de>
2018-10-16 08:26:37 +0200

    Added class documentation for the SecureStringExtensions class

--- BinaryTools/Extensions/SecureStringExtensions.cs
+++ BinaryTools/Extensions/SecureStringExtensions.cs
@@ -3,6 +3,10 @@ using System.Runtime.InteropServices;
 using System.Security;
 
 namespace BinaryTools.Extensions {
+
+   /// <summary>
+   /// A collection of helpful extension methods for the <see cref="SecureString"/> class
+   /// </summary>
    public static class SecureStringExtensions {
 
       public static string ToUnsecureString(this SecureString secStr) {
