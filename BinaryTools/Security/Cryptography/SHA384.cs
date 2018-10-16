82bcc3124d01b642a8372c0a1bd4e95ffd8b3da4
Lukas Eßmann <l.essmann@directonline.de>
2018-10-16 07:36:30 +0200

    Added class BinaryTools.Security.Cryptography.SHA384

---
+++ BinaryTools/Security/Cryptography/SHA384.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTools.Security.Cryptography {
+   public static class SHA384 {
+   }
+}
