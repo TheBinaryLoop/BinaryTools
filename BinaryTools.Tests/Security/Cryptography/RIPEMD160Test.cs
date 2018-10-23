394bc955e7e382b8a0532e1634a7f4ebe1ce5dc4
Lukas Eßmann <l.essmann@directonline.de>
2018-10-23 10:22:41 +0200

    Moved RIPEMD160Test class into the right namspace Added .editorconfig

---
+++ BinaryTools.Tests/Security/Cryptography/RIPEMD160Test.cs
@@ -0,0 +1,16 @@
+﻿using BinaryTools.Security.Cryptography;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BinaryTools.Tests.Security.Cryptography
+{
+    [TestClass]
+    public class RIPEMD160Test
+    {
+        [TestMethod]
+        public void TestComputeHashFromString()
+        {
+            string testString = "This is the test string";
+            Assert.AreEqual(RIPEMD160.ComputeHash(testString), "261733CAE4EBD91AE9603C0804F0BF6F96EDCF0A");
+        }
+    }
+}
