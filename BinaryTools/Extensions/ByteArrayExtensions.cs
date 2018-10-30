943a42fecdfb410936a46230b7df5b84b3fa6f4c
Lukas Eßmann <l.essmann@directonline.de>
2018-10-30 13:44:14 +0100

    Added extension class BinaryTools.Extensions.ByteArrayExtensions Added extension method Find to ByteArrayExtensions

---
+++ BinaryTools/Extensions/ByteArrayExtensions.cs
@@ -0,0 +1,30 @@
+﻿namespace BinaryTools.Extensions
+{
+    public static class ByteArrayExtensions
+    {
+        public static int Find(this byte[] src, byte[] find, int startIndex = 0)
+        {
+            int index = -1;
+            int matchIndex = 0;
+            // handle the complete source array
+            for (int i = startIndex; i < src.Length; i++)
+            {
+                if (src[i] == find[matchIndex])
+                {
+                    if (matchIndex == (find.Length - 1))
+                    {
+                        index = i - matchIndex;
+                        break;
+                    }
+                    matchIndex++;
+                }
+                else
+                {
+                    matchIndex = 0;
+                }
+
+            }
+            return index;
+        }
+    }
+}
