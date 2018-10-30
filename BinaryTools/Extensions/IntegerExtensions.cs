58315da404d66bd0c244ba17b977e1d98ebee451
Lukas Eßmann <l.essmann@directonline.de>
2018-10-30 09:40:42 +0100

    Added extension class BinaryTools.Extensions.IntegerExtensions Added extension method ToHex to the IntegerExtensions class

---
+++ BinaryTools/Extensions/IntegerExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTools.Extensions
+{
+    public static class IntegerExtensions
+    {
+        public static string ToHex(this int value)
+        {
+            return String.Format("0x{0:X}", value);
+        }
+    }
+}
