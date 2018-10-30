d4d575b1738cd295b4daf125ed0d3febb6c8e096
Lukas Eßmann <l.essmann@directonline.de>
2018-10-30 13:52:01 +0100

    Added extension class BinaryTools.Extensions.ListExtensions Added extension method Push to the ListExtensions class Added extension method Pop to the ListExtensions class

---
+++ BinaryTools/Extensions/ListExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace BinaryTools.Extensions
+{
+    public static class ListExtensions
+    {
+        public static T Pop<T>(this List<T> list)
+        {
+            T f = list[0];
+            list.Remove(f);
+            return f;
+        }
+        public static void Push<T>(this List<T> list, T item)
+        {
+            list.Add(item);
+        }
+    }
+}
