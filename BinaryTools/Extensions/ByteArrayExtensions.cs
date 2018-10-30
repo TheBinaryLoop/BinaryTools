fb5aecc70a3be6be0888d8bc686bf21823955283
Lukas Eßmann <l.essmann@directonline.de>
2018-10-30 15:11:46 +0100

    Added extension method DecompressGZip to the ByteArrayExtensions class

--- BinaryTools/Extensions/ByteArrayExtensions.cs
+++ BinaryTools/Extensions/ByteArrayExtensions.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.IO.Compression;
 using System.Text;
 
 namespace BinaryTools.Extensions
@@ -166,5 +168,65 @@ namespace BinaryTools.Extensions
                 src.Length - (start + (end - start)));
             return dst;
         }
+
+        /// <summary>
+        /// Decompresses the byte array with gzip to a <see cref="string"/>.
+        /// </summary>
+        /// <param name="src">The byte array to act on.</param>
+        /// <returns>The decompressed string.</returns>
+        public static string DecompressGZip(this byte[] src)
+        {
+            const int bufferSize = 1024;
+            using (var memoryStream = new MemoryStream(src))
+            {
+                using (var zipStream = new GZipStream(memoryStream, CompressionMode.Decompress))
+                {
+                    // Memory stream for storing the decompressed bytes
+                    using (var outStream = new MemoryStream())
+                    {
+                        var buffer = new byte[bufferSize];
+                        int totalBytes = 0;
+                        int readBytes;
+                        while ((readBytes = zipStream.Read(buffer, 0, bufferSize)) > 0)
+                        {
+                            outStream.Write(buffer, 0, readBytes);
+                            totalBytes += readBytes;
+                        }
+                        return Encoding.Default.GetString(outStream.GetBuffer(), 0, totalBytes);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Decompresses the byte array with gzip to a <see cref="string"/>.
+        /// </summary>
+        /// <param name="src">The byte array to act on.</param>
+        /// <param name="encoding">The <see cref="Encoding"/> to use.</param>
+        /// <returns>The decompressed string.</returns>
+        public static string DecompressGZip(this byte[] src, Encoding encoding)
+        {
+            const int bufferSize = 1024;
+            using (var memoryStream = new MemoryStream(src))
+            {
+                using (var zipStream = new GZipStream(memoryStream, CompressionMode.Decompress))
+                {
+                    // Memory stream for storing the decompressed bytes
+                    using (var outStream = new MemoryStream())
+                    {
+                        var buffer = new byte[bufferSize];
+                        int totalBytes = 0;
+                        int readBytes;
+                        while ((readBytes = zipStream.Read(buffer, 0, bufferSize)) > 0)
+                        {
+                            outStream.Write(buffer, 0, readBytes);
+                            totalBytes += readBytes;
+                        }
+                        return encoding.GetString(outStream.GetBuffer(), 0, totalBytes);
+                    }
+                }
+            }
+        }
+
     }
 }
