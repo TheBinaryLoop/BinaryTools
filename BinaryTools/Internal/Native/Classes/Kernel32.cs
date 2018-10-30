17ba9046522325015832bc004fbf718cc76fd5b7
Lukas Eßmann <l.essmann@directonline.de>
2018-10-30 09:21:04 +0100

    Added class BinaryTools.Internal.Native.Classes.Kernel32

---
+++ BinaryTools/Internal/Native/Classes/Kernel32.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.InteropServices;
+using System.Text;
+
+namespace BinaryTools.Internal.Native.Classes
+{
+    internal class Kernel32
+    {
+        [DllImport("kernel32", CharSet = CharSet.Unicode)]
+        internal static extern long WritePrivateProfileString(string Section, string Key, string Value, string FilePath);
+
+        [DllImport("kernel32", CharSet = CharSet.Unicode)]
+        internal static extern int GetPrivateProfileString(string Section, string Key, string Default, StringBuilder RetVal, int Size, string FilePath);
+    }
+}
