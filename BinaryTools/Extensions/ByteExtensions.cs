dc1f59b9134d2f33a570aad30422a4bf2a2b42e5
Lukas Eßmann <l.essmann@directonline.de>
2018-10-31 13:46:47 +0100

    Added extension class BinaryTools.Extensions.ByteExtensions

---
+++ BinaryTools/Extensions/ByteExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BinaryTools.Extensions
+{
+    /// <summary>
+    /// A collection of helpful extension methods for the <see cref="Byte"/> class.
+    /// </summary>
+    public static class ByteExtensions
+    {
+
+    }
+}
