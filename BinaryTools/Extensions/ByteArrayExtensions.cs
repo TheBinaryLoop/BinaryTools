44facba7a67a48a92626e6f395b2e34334b666f1
Lukas Eßmann <l.essmann@directonline.de>
2018-10-31 15:00:13 +0100

    Added extension method ToBase64CharArray to the ByteArrayExtensions class

--- BinaryTools/Extensions/ByteArrayExtensions.cs
+++ BinaryTools/Extensions/ByteArrayExtensions.cs
@@ -258,6 +258,23 @@ namespace BinaryTools.Extensions
         }
 
         /// <summary>
+        /// Converts a subset of an 8-bit unsigned integer array to an equivalent subset of a Unicode character array encoded with base-64 
+        /// digits. Parameters specify the subsets as offsets in the input and output arrays, the number of elements in the input array to 
+        /// convert, and whether line breaks are inserted in the output array.
+        /// </summary>
+        /// <param name="inArray">An input array of 8-bit unsigned integers.</param>
+        /// <param name="offsetIn">A position within inArray.</param>
+        /// <param name="length">The number of elements of inArray to convert.</param>
+        /// <param name="outArray">An output array of Unicode characters.</param>
+        /// <param name="offsetOut">A position within outArray.</param>
+        /// <param name="options">InsertLineBreaks to insert a line break every 76 characters, or None to not insert line breaks.</param>
+        /// <returns>A 32-bit signed integer containing the number of bytes in outArray.</returns>
+        public static Int32 ToBase64CharArray(this Byte[] inArray, Int32 offsetIn, Int32 length, Char[] outArray, Int32 offsetOut, Base64FormattingOptions options)
+        {
+            return Convert.ToBase64CharArray(inArray, offsetIn, length, outArray, offsetOut, options);
+        }
+
+        /// <summary>
         /// Converts the byte array to an image.
         /// </summary>
         /// <param name="src">The byte array to act on.</param>
@@ -279,9 +296,6 @@ namespace BinaryTools.Extensions
         {
             return new MemoryStream(src);
         }
-
-
-
 
     }
 }
