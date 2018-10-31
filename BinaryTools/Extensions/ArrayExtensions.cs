f8ea9d5215de115520dde350d84b927a30f4ed00
Lukas Eßmann <l.essmann@directonline.de>
2018-10-31 11:41:11 +0100

    Added extension method ConstrainedCopy to the ArrayExtensions class

--- BinaryTools/Extensions/ArrayExtensions.cs
+++ BinaryTools/Extensions/ArrayExtensions.cs
@@ -113,6 +113,20 @@ namespace BinaryTools.Extensions
         }
 
         /// <summary>
+        /// Copies a range of elements from an <see cref="Array"/> starting at the specified source index and pastes them to another <see cref="Array"/> starting at the 
+        /// specified destination index. Guarantees that all changes are undone if the copy does not succeed completely.
+        /// </summary>
+        /// <param name="sourceArray">The <see cref="Array"/> that contains the data to copy.</param>
+        /// <param name="sourceIndex">A 32-bit integer that represents the index in the sourceArray at which copying begins.</param>
+        /// <param name="destinationArray">The <see cref="Array"/> that receives the data.</param>
+        /// <param name="destinationIndex">A 32-bit integer that represents the index in the destinationArray at which storing begins.</param>
+        /// <param name="length">A 32-bit integer that represents the number of elements to copy.</param>
+        public static void ConstrainedCopy(this Array sourceArray, Int32 sourceIndex, Array destinationArray, Int32 destinationIndex, Int32 length)
+        {
+            Array.ConstrainedCopy(sourceArray, sourceIndex, destinationArray, destinationIndex, length);
+        }
+
+        /// <summary>
         /// Checks if the array is lower then the specified index.
         /// </summary>
         /// <param name="array">The array to act on.</param>
