876bc8f14934235779629b0c1345e5fc27e020d7
Lukas Eßmann <l.essmann@directonline.de>
2018-10-31 13:16:47 +0100

    Added extension method Sort to the ArrayExtensions class

--- BinaryTools/Extensions/ArrayExtensions.cs
+++ BinaryTools/Extensions/ArrayExtensions.cs
@@ -293,6 +293,130 @@ namespace BinaryTools.Extensions
         }
 
         /// <summary>
+        /// Sorts the elements in an entire one-dimensional <see cref="Array"/> using the <see cref="IComparable"/> implementation of each element of the <see cref="Array"/>.
+        /// </summary>
+        /// <param name="array">The one-dimensional <see cref="Array"/> to sort.</param>
+        public static void Sort(this Array array)
+        {
+            Array.Sort(array);
+        }
+
+        /// <summary>
+        /// Sorts a pair of one-dimensional <see cref="Array"/> objects (one contains the keys and the other contains the corresponding items) based on 
+        /// the keys in the first <see cref="Array"/> using the <see cref="IComparable"/> implementation of each key.
+        /// </summary>
+        /// <param name="array">The one-dimensional <see cref="Array"/> to sort.</param>
+        /// <param name="items">
+        /// The one-dimensional <see cref="Array"/> that contains the items that correspond to each of the keys in the keys<see cref="Array"/>.
+        /// -or-
+        /// null to sort only the keysArray.
+        /// </param>
+        public static void Sort(this Array array, Array items)
+        {
+            Array.Sort(array, items);
+        }
+
+        /// <summary>
+        /// Sorts the elements in a range of elements in a one-dimensional <see cref="Array"/> using the <see cref="IComparable"/> implementation of each element of the <see cref="Array"/>.
+        /// </summary>
+        /// <param name="array">The one-dimensional <see cref="Array"/> to sort.</param>
+        /// <param name="index">The starting index of the range to sort.</param>
+        /// <param name="length">The number of elements in the range to sort.</param>
+        public static void Sort(this Array array, Int32 index, Int32 length)
+        {
+            Array.Sort(array, index, length);
+        }
+
+        /// <summary>
+        /// Sorts a range of elements in a pair of one-dimensional <see cref="Array"/> objects (one contains the keys and the other contains the 
+        /// corresponding items) based on the keys in the first <see cref="Array"/> using the <see cref="IComparable"/> implementation of each key.
+        /// </summary>
+        /// <param name="array">The one-dimensional <see cref="Array"/> to sort.</param>
+        /// <param name="items">
+        /// The one-dimensional <see cref="Array"/> that contains the items that correspond to each of the keys in the keys<see cref="Array"/>.
+        /// -or-
+        /// null to sort only the keysArray.
+        /// </param>
+        /// <param name="index">The starting index of the range to sort.</param>
+        /// <param name="length">The number of elements in the range to sort.</param>
+        public static void Sort(this Array array, Array items, Int32 index, Int32 length)
+        {
+            Array.Sort(array, items, index, length);
+        }
+
+        /// <summary>
+        /// Sorts the elements in a one-dimensional <see cref="Array"/> using the specified <see cref="IComparer"/>.
+        /// </summary>
+        /// <param name="array">The one-dimensional <see cref="Array"/> to sort.</param>
+        /// <param name="comparer">
+        /// The implementation to use when comparing elements. 
+        /// -or-
+        /// null to use the <see cref="IComparable"/> implementation of each element.
+        /// </param>
+        public static void Sort(this Array array, IComparer comparer)
+        {
+            Array.Sort(array, comparer);
+        }
+
+        /// <summary>
+        /// Sorts a pair of one-dimensional <see cref="Array"/> objects (one contains the keys and the other contains the corresponding items) based on 
+        /// the keys in the first <see cref="Array"/> using the specified <see cref="IComparer"/>.
+        /// </summary>
+        /// <param name="array">The one-dimensional <see cref="Array"/> to sort.</param>
+        /// <param name="items">
+        /// The one-dimensional <see cref="Array"/> that contains the items that correspond to each of the keys in the keys<see cref="Array"/>.
+        /// -or-
+        /// null to sort only the keysArray.
+        /// </param>
+        /// <param name="comparer">
+        /// The <see cref="IComparer"/> implementation to use when comparing elements. 
+        /// -or-
+        /// null to use the <see cref="IComparable"/> implementation of each element.
+        /// </param>
+        public static void Sort(this Array array, Array items, IComparer comparer)
+        {
+            Array.Sort(array, items, comparer);
+        }
+
+        /// <summary>
+        /// Sorts the elements in a range of elements in a one-dimensional <see cref="Array"/> using the specified <see cref="IComparer"/>.
+        /// </summary>
+        /// <param name="array">The one-dimensional <see cref="Array"/> to sort.</param>
+        /// <param name="index">The starting index of the range to sort.</param>
+        /// <param name="length">The number of elements in the range to sort.</param>
+        /// <param name="comparer">
+        /// The <see cref="IComparer"/> implementation to use when comparing elements. 
+        /// -or-
+        /// null to use the <see cref="IComparable"/> implementation of each element.
+        /// </param>
+        public static void Sort(this Array array, Int32 index, Int32 length, IComparer comparer)
+        {
+            Array.Sort(array, index, length, comparer);
+        }
+
+        /// <summary>
+        /// Sorts a range of elements in a pair of one-dimensional <see cref="Array"/> objects (one contains the keys and the other contains the 
+        /// corresponding items) based on the keys in the first <see cref="Array"/> using the specified <see cref="IComparer"/>.
+        /// </summary>
+        /// <param name="array">The one-dimensional <see cref="Array"/> to sort.</param>
+        /// <param name="items">
+        /// The one-dimensional <see cref="Array"/> that contains the items that correspond to each of the keys in the keys<see cref="Array"/>.
+        /// -or-
+        /// null to sort only the keysArray.
+        /// </param>
+        /// <param name="index">The starting index of the range to sort.</param>
+        /// <param name="length">The number of elements in the range to sort.</param>
+        /// <param name="comparer">
+        /// The <see cref="IComparer"/> implementation to use when comparing elements. 
+        /// -or-
+        /// null to use the <see cref="IComparable"/> implementation of each element.
+        /// </param>
+        public static void Sort(this Array array, Array items, Int32 index, Int32 length, IComparer comparer)
+        {
+            Array.Sort(array, items, index, length, comparer);
+        }
+
+        /// <summary>
         /// Checks if the array is lower then the specified index.
         /// </summary>
         /// <param name="array">The array to act on.</param>
