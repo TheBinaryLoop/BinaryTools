d149286b0b1fea0c95556874fb1a195293011d8b
Lukas Eßmann <l.essmann@directonline.de>
2018-10-31 13:30:51 +0100

    Added extension method ByteLength to the BufferExtensions class

--- BinaryTools/Extensions/BufferExtensions.cs
+++ BinaryTools/Extensions/BufferExtensions.cs
@@ -19,5 +19,16 @@ namespace BinaryTools.Extensions
         {
             Buffer.BlockCopy(src, srcOffset, dst, dstOffset, count);
         }
+
+        /// <summary>
+        /// Returns the number of bytes in the specified array.
+        /// </summary>
+        /// <param name="array">An array.</param>
+        /// <returns>The number of bytes in the array.</returns>
+        public static Int32 ByteLength(this Array array)
+        {
+            return Buffer.ByteLength(array);
+        }
+
     }
 }
