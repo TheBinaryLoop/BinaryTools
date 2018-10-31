083a3848b0ae5bf3a400e5fc2e4f59148e9ae241
Lukas Eßmann <l.essmann@directonline.de>
2018-10-31 15:23:00 +0100

    Added extension method UrlEncode to the ByteArrayExtension class

--- BinaryTools/Extensions/ByteArrayExtensions.cs
+++ BinaryTools/Extensions/ByteArrayExtensions.cs
@@ -412,6 +412,33 @@ namespace BinaryTools.Extensions
 
         #endregion
 
+        #region UrlEncode
+
+        /// <summary>
+        /// Converts a byte array into an encoded URL string.
+        /// </summary>
+        /// <param name="bytes">The array of bytes to encode.</param>
+        /// <returns>An encoded string.</returns>
+        public static String UrlEncode(this Byte[] bytes)
+        {
+            return HttpUtility.UrlEncode(bytes);
+        }
+
+        /// <summary>
+        /// Converts a byte array into a URL-encoded string, starting at the specified position in the array and continuing for the specified 
+        /// number of bytes.
+        /// </summary>
+        /// <param name="bytes">The array of bytes to encode.</param>
+        /// <param name="offset">The position in the byte array at which to begin encoding.</param>
+        /// <param name="count">The number of bytes to encode.</param>
+        /// <returns>An encoded string.</returns>
+        public static String UrlEncode(this Byte[] bytes, Int32 offset, Int32 count)
+        {
+            return HttpUtility.UrlEncode(bytes, offset, count);
+        }
+
+        #endregion
+
         /// <summary>
         /// Encodes a byte array into its equivalent string representation using base 64 digits, which is usable for transmission on the URL.
         /// </summary>
