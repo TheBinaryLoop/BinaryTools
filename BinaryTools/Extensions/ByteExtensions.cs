00ca6f75cf2ffe55532468bd00bc767f64dc4f10
Lukas Eßmann <l.essmann@directonline.de>
2018-10-31 14:14:31 +0100

    Added extension method Min to the ByteExtensions class

--- BinaryTools/Extensions/ByteExtensions.cs
+++ BinaryTools/Extensions/ByteExtensions.cs
@@ -18,5 +18,16 @@ namespace BinaryTools.Extensions
             return Math.Max(val1, val2);
         }
 
+        /// <summary>
+        /// Returns the smaller of two 8-bit unsigned integers.
+        /// </summary>
+        /// <param name="val1">The first of two 8-bit unsigned integers to compare.</param>
+        /// <param name="val2">The second of two 8-bit unsigned integers to compare.</param>
+        /// <returns>Parameter val1 or val2, whichever is smaller.</returns>
+        public static Byte Min(this Byte val1, Byte val2)
+        {
+            return Math.Min(val1, val2);
+        }
+
     }
 }
