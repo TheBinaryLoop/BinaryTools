81893947b65a441c9a3d9167f3a9afd5a4fbb437
Lukas Eßmann <l.essmann@directonline.de>
2018-10-31 08:47:01 +0100

    Renamed the IntegerExtensions class to Int32Extensions

---
+++ BinaryTools/Extensions/Int32Extensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BinaryTools.Extensions
+{
+    /// <summary>
+    /// A collection of helpful extension methods for the <see cref="Int32"/> class.
+    /// </summary>
+    public static class Int32Extensions
+    {
+        public static string ToHex(this Int32 value)
+        {
+            return String.Format("0x{0:X}", value);
+        }
+    }
+}
