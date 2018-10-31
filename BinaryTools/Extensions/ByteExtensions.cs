d6dc7d1e51eaf0bceddd77867c3fa0bd7c977c81
Lukas Eßmann <l.essmann@directonline.de>
2018-10-31 14:07:52 +0100

    Added extension method Max to the ByteExtension class

--- BinaryTools/Extensions/ByteExtensions.cs
+++ BinaryTools/Extensions/ByteExtensions.cs
@@ -7,6 +7,16 @@ namespace BinaryTools.Extensions
     /// </summary>
     public static class ByteExtensions
     {
+        /// <summary>
+        /// Returns the larger of two 8-bit unsigned integers.
+        /// </summary>
+        /// <param name="val1">The first of two 8-bit unsigned integers to compare.</param>
+        /// <param name="val2">The second of two 8-bit unsigned integers to compare.</param>
+        /// <returns>Parameter val1 or val2, whichever is larger.</returns>
+        public static Byte Max(this Byte val1, Byte val2)
+        {
+            return Math.Max(val1, val2);
+        }
 
     }
 }
