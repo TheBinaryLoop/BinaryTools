024226d9b1e45359ba854b139122e76ad64d882a
Lukas Eßmann <l.essmann@directonline.de>
2018-10-31 12:53:33 +0100

    Added extension method LastIndexOf to the ArrayExtensions class

--- BinaryTools/Extensions/ArrayExtensions.cs
+++ BinaryTools/Extensions/ArrayExtensions.cs
@@ -227,6 +227,52 @@ namespace BinaryTools.Extensions
         }
 
         /// <summary>
+        /// Searches for the specified object and returns the index of the last occurrence within the entire one-dimensional <see cref="Array"/>.
+        /// </summary>
+        /// <param name="array">The one-dimensional <see cref="Array"/> to search.</param>
+        /// <param name="value">The object to locate in array.</param>
+        /// <returns>
+        /// The index of the last occurrence of value within the entire array, if found; otherwise, the lower bound of the array minus 1.
+        /// </returns>
+        public static Int32 LastIndexOf(this Array array, Object value)
+        {
+            return Array.LastIndexOf(array, value);
+        }
+
+        /// <summary>
+        /// Searches for the specified object and returns the index of the last occurrence within the range of elements in the one-dimensional 
+        /// <see cref="Array"/> that extends from the first element to the specified index.
+        /// </summary>
+        /// <param name="array">The one-dimensional <see cref="Array"/> to search.</param>
+        /// <param name="value">The object to locate in array.</param>
+        /// <param name="startIndex">The starting index of the backward search.</param>
+        /// <returns>
+        /// The index of the last occurrence of value within the range of elements in array that extends from the first element to 
+        /// startIndex, if found; otherwise, the lower bound of the array minus 1.
+        /// </returns>
+        public static Int32 LastIndexOf(this Array array, Object value, Int32 startIndex)
+        {
+            return Array.LastIndexOf(array, value, startIndex);
+        }
+
+        /// <summary>
+        /// Searches for the specified object and returns the index of the last occurrence within the range of elements in the one-dimensional 
+        /// <see cref="Array"/> that contains the specified number of elements and ends at the specified index.
+        /// </summary>
+        /// <param name="array">The one-dimensional <see cref="Array"/> to search.</param>
+        /// <param name="value">The object to locate in array.</param>
+        /// <param name="startIndex">The starting index of the backward search.</param>
+        /// <param name="count">The number of elements in the section to search.</param>
+        /// <returns>
+        /// The index of the last occurrence of value within the range of elements in array that contains the number of elements specified 
+        /// in count and ends at startIndex, if found; otherwise, the lower bound of the array minus 1.
+        /// </returns>
+        public static Int32 LastIndexOf(this Array array, Object value, Int32 startIndex, Int32 count)
+        {
+            return Array.LastIndexOf(array, value, startIndex, count);
+        }
+
+        /// <summary>
         /// Checks if the array is lower then the specified index.
         /// </summary>
         /// <param name="array">The array to act on.</param>
