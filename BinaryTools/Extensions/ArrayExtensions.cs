0b9763cc42574aec996376cc9616f0f9484e68b0
Lukas Eßmann <l.essmann@directonline.de>
2018-10-31 11:49:06 +0100

    Added extension method Copy to the ArrayExtensions class

--- BinaryTools/Extensions/ArrayExtensions.cs
+++ BinaryTools/Extensions/ArrayExtensions.cs
@@ -127,6 +127,62 @@ namespace BinaryTools.Extensions
         }
 
         /// <summary>
+        /// Copies a range of elements from an <see cref="Array"/> starting at the first element and pastes them into another <see cref="Array"/> starting at the first
+        /// element. The length is specified as a 32-bit integer.
+        /// </summary>
+        /// <param name="sourceArray">The <see cref="Array"/> that contains the data to copy.</param>
+        /// <param name="destinationArray">The <see cref="Array"/> that receives the data.</param>
+        /// <param name="length">A 32-bit integer that represents the number of elements to copy.</param>
+        public static void Copy(this Array sourceArray, Array destinationArray, Int32 length)
+        {
+            Array.Copy(sourceArray, destinationArray, length);
+        }
+
+        /// <summary>
+        /// Copies a range of elements from an <see cref="Array"/> starting at the specified source index and pastes them to another <see cref="Array"/> starting at the
+        /// specified destination index. The length and the indexes are specified as 32-bit integers.
+        /// </summary>
+        /// <param name="sourceArray">The <see cref="Array"/> that contains the data to copy.</param>
+        /// <param name="sourceIndex">A 32-bit integer that represents the index in the  at which copying begins.</param>
+        /// <param name="destinationArray">The <see cref="Array"/> that receives the data.</param>
+        /// <param name="destinationIndex">A 32-bit integer that represents the index in the  at which storing begins.</param>
+        /// <param name="length">A 32-bit integer that represents the number of elements to copy.</param>
+        public static void Copy(this Array sourceArray, Int32 sourceIndex, Array destinationArray, Int32 destinationIndex, Int32 length)
+        {
+            Array.Copy(sourceArray, sourceIndex, destinationArray, destinationIndex, length);
+        }
+
+        /// <summary>
+        /// Copies a range of elements from an <see cref="Array"/> starting at the first element and pastes them into another <see cref="Array"/> starting at the first
+        /// element. The length is specified as a 64-bit integer.
+        /// </summary>
+        /// <param name="sourceArray">The <see cref="Array"/> that contains the data to copy.</param>
+        /// <param name="destinationArray">The <see cref="Array"/> that receives the data.</param>
+        /// <param name="length">
+        /// A 64-bit integer that represents the number of elements to copy. The integer must be between zero and <see cref="Int32.MaxValue"/>, inclusive.
+        /// </param>
+        public static void Copy(this Array sourceArray, Array destinationArray, Int64 length)
+        {
+            Array.Copy(sourceArray, destinationArray, length);
+        }
+
+        /// <summary>
+        /// Copies a range of elements from an <see cref="Array"/> starting at the specified source index and pastes them to another <see cref="Array"/> starting at the
+        /// specified destination index. The length and the indexes are specified as 64-bit integers.
+        /// </summary>
+        /// <param name="sourceArray">The <see cref="Array"/> that contains the data to copy.</param>
+        /// <param name="sourceIndex">A 64-bit integer that represents the index in the sourceArray at which copying begins.</param>
+        /// <param name="destinationArray">The <see cref="Array"/> that receives the data.</param>
+        /// <param name="destinationIndex">A 64-bit integer that represents the index in the destinationArray at which storing begins.</param>
+        /// <param name="length">
+        /// A 64-bit integer that represents the number of elements to copy. The integer must be between zero and <see cref="Int32.MaxValue"/>, inclusive.
+        /// </param>
+        public static void Copy(this Array sourceArray, Int64 sourceIndex, Array destinationArray, Int64 destinationIndex, Int64 length)
+        {
+            Array.Copy(sourceArray, sourceIndex, destinationArray, destinationIndex, length);
+        }
+
+        /// <summary>
         /// Checks if the array is lower then the specified index.
         /// </summary>
         /// <param name="array">The array to act on.</param>
