08e3a33301789e071a95fac57c1756a5f3ac760a
Lukas Eßmann <l.essmann@directonline.de>
2018-10-31 11:53:43 +0100

    Added extension method IndexOf to the ArrayExtensions method

--- BinaryTools/Extensions/ArrayExtensions.cs
+++ BinaryTools/Extensions/ArrayExtensions.cs
@@ -183,6 +183,50 @@ namespace BinaryTools.Extensions
         }
 
         /// <summary>
+        /// Searches for the specified object and returns the index of its first occurrence in a one-dimensional array.
+        /// </summary>
+        /// <param name="array">The one-dimensional array to search.</param>
+        /// <param name="value">The object to locate in array.</param>
+        /// <returns>The index of the first occurrence of value in array, if found; otherwise, the lower bound of the array minus 1.</returns>
+        public static Int32 IndexOf(this Array array, Object value)
+        {
+            return Array.IndexOf(array, value);
+        }
+
+        /// <summary>
+        /// Searches for the specified object in a range of elements of a one-dimensional array, and returns the index of its first occurrence. 
+        /// The range extends from a specified index to the end of the array.
+        /// </summary>
+        /// <param name="array">The one-dimensional array to search.</param>
+        /// <param name="value">The object to locate in array.</param>
+        /// <param name="startIndex">The starting index of the search. 0 (zero) is valid in an empty array.</param>
+        /// <returns>
+        /// The index of the first occurrence of value, if it’s found, within the range of elements in array that extends from startIndex to 
+        /// the last element; otherwise, the lower bound of the array minus 1.
+        /// </returns>
+        public static Int32 IndexOf(this Array array, Object value, Int32 startIndex)
+        {
+            return Array.IndexOf(array, value, startIndex);
+        }
+
+        /// <summary>
+        /// Searches for the specified object in a range of elements of a one-dimensional array, and returns the index of ifs first occurrence. 
+        /// The range extends from a specified index for a specified number of elements.
+        /// </summary>
+        /// <param name="array">The one-dimensional array to search.</param>
+        /// <param name="value">The object to locate in array.</param>
+        /// <param name="startIndex">The starting index of the search. 0 (zero) is valid in an empty array.</param>
+        /// <param name="count">The number of elements to search.</param>
+        /// <returns>
+        /// The index of the first occurrence of value, if it’s found in the array from index startIndex to startIndex + count - 1; 
+        /// otherwise, the lower bound of the array minus 1.
+        /// </returns>
+        public static Int32 IndexOf(this Array array, Object value, Int32 startIndex, Int32 count)
+        {
+            return Array.IndexOf(array, value, startIndex, count);
+        }
+
+        /// <summary>
         /// Checks if the array is lower then the specified index.
         /// </summary>
         /// <param name="array">The array to act on.</param>
