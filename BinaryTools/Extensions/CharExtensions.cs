8fb173fd224b2e1a48459ae44530378309df6670
Lukas Eßmann <l.essmann@directonline.de>
2018-10-31 15:38:22 +0100

    Added extension method ConvertToUtf32 to the CharExtensions class

--- BinaryTools/Extensions/CharExtensions.cs
+++ BinaryTools/Extensions/CharExtensions.cs
@@ -10,6 +10,17 @@ namespace BinaryTools.Extensions
     public static class CharExtensions
     {
         /// <summary>
+        /// Converts the value of a UTF-16 encoded surrogate pair into a Unicode code point.
+        /// </summary>
+        /// <param name="highSurrogate">A high surrogate code unit (that is, a code unit ranging from U+D800 through U+DBFF).</param>
+        /// <param name="lowSurrogate">A low surrogate code unit (that is, a code unit ranging from U+DC00 through U+DFFF).</param>
+        /// <returns>The 21-bit Unicode code point represented by the highSurrogate and lowSurrogate parameters.</returns>
+        public static Int32 ConvertToUtf32(this Char highSurrogate, Char lowSurrogate)
+        {
+            return Char.ConvertToUtf32(highSurrogate, lowSurrogate);
+        }
+
+        /// <summary>
         /// Repeats a character the specified number of times.
         /// </summary>
         /// <param name="src">The char to act on.</param>
