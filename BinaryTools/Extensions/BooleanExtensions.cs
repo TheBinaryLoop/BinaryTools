d6a49d372aace8f411bdcf10f7fe859d402f07e2
Lukas Eßmann <l.essmann@directonline.de>
2018-10-31 13:36:43 +0100

    Added new extension class BinaryTools.Extensions.BooleanExtensions

---
+++ BinaryTools/Extensions/BooleanExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BinaryTools.Extensions
+{
+    /// <summary>
+    /// A collection of helpful extension methods for the <see cref="Boolean"/> class.
+    /// </summary>
+    public static class BooleanExtensions
+    {
+
+    }
+}
