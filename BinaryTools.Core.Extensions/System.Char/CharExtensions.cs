3d2c6765ed6a5c6548f83b1899be0c434c2255a7
Lukas Eßmann <l.essmann@directonline.de>
2018-11-05 13:21:36 +0100

    Added extension method ToLowerInvariant  to the CharExtensions class

--- BinaryTools.Core.Extensions/System.Char/CharExtensions.cs
+++ BinaryTools.Core.Extensions/System.Char/CharExtensions.cs
@@ -232,6 +232,16 @@ namespace BinaryTools.Core.Extensions
         #endregion
 
         /// <summary>
+        /// Converts the value of this Unicode character to its lowercase equivalent using the casing rules of the invariant culture.
+        /// </summary>
+        /// <param name="c">The Unicode character to convert.</param>
+        /// <returns>The lowercase equivalent of the c parameter, or the unchanged value of c, if c is already lowercase or not alphabetic.</returns>
+        public static Char ToLowerInvariant(this Char c)
+        {
+            return Char.ToLowerInvariant(c);
+        }
+
+        /// <summary>
         /// Repeats a character the specified number of times.
         /// </summary>
         /// <param name="src">The char to act on.</param>
