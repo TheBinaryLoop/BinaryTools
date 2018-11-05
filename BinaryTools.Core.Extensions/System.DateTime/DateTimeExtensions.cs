b431dc40afadfc1863a72876ddf1c7ec8353eff3
Lukas Eßmann <l.essmann@directonline.de>
2018-11-05 13:26:18 +0100

    Added extension class BinaryTools.Core.Extensions.DateTimeExtensions

---
+++ BinaryTools.Core.Extensions/System.DateTime/DateTimeExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BinaryTools.Core.Extensions
+{
+    /// <summary>
+    /// A collection of helpful extension methods for the <see cref="DateTime"/> class.
+    /// </summary>
+    public static partial class DateTimeExtensions
+    {
+
+    }
+}
