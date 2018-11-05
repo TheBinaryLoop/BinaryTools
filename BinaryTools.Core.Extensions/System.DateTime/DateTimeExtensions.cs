cd102de481cfe55c75a2e8a99e21a16a1f9186a5
Lukas Eßmann <l.essmann@directonline.de>
2018-11-05 13:34:13 +0100

    Added extension method GetAge to the DateTimeExtensions class

--- BinaryTools.Core.Extensions/System.DateTime/DateTimeExtensions.cs
+++ BinaryTools.Core.Extensions/System.DateTime/DateTimeExtensions.cs
@@ -8,5 +8,20 @@ namespace BinaryTools.Core.Extensions
     public static partial class DateTimeExtensions
     {
 
+        /// <summary>
+        /// Gets the age of the current DateTime object until now in years.
+        /// </summary>
+        /// <param name="src">The DateTime object to act on.</param>
+        /// <returns>The age of the current DateTime object until now in years.</returns>
+        public static Int32 GetAge(this DateTime src)
+        {
+            if (DateTime.Today.Month < src.Month ||
+                DateTime.Today.Month == src.Month &&
+                DateTime.Today.Day < src.Day)
+            {
+                return DateTime.Today.Year - src.Year - 1;
+            }
+            return DateTime.Today.Year - src.Year;
+        }
     }
 }
