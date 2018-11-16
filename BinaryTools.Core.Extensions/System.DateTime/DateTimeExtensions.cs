a34078627dee336cc7a27e620d7718b8e311780e
Lukas Eßmann <l.essmann@binarypanther.com>
2018-11-16 08:12:44 +0100

    Added System.DateTime extension methods
    - Added EndOfMonth method
    - Added EndOfWeek method
    - Added EndOfYear method

--- BinaryTools.Core.Extensions/System.DateTime/DateTimeExtensions.cs
+++ BinaryTools.Core.Extensions/System.DateTime/DateTimeExtensions.cs
@@ -11,37 +11,87 @@ namespace BinaryTools.Core.Extensions
         /// <summary>
         /// Gets the age of the current DateTime object until now in years.
         /// </summary>
-        /// <param name="src">The DateTime object to act on.</param>
+        /// <param name="dateTime">The DateTime object to act on.</param>
         /// <returns>The age of the current DateTime object until now in years.</returns>
-        public static Int32 GetAge(this DateTime src)
+        public static Int32 GetAge(this DateTime dateTime)
         {
-            if (DateTime.Today.Month < src.Month ||
-                DateTime.Today.Month == src.Month &&
-                DateTime.Today.Day < src.Day)
+            if (DateTime.Today.Month < dateTime.Month ||
+                DateTime.Today.Month == dateTime.Month &&
+                DateTime.Today.Day < dateTime.Day)
             {
-                return DateTime.Today.Year - src.Year - 1;
+                return DateTime.Today.Year - dateTime.Year - 1;
             }
-            return DateTime.Today.Year - src.Year;
+            return DateTime.Today.Year - dateTime.Year;
         }
 
         /// <summary>
         /// Gets the elapsed time between this DateTime object and now.
         /// </summary>
-        /// <param name="datetime">The DateTime object to act on.</param>
+        /// <param name="dateTime">The DateTime object to act on.</param>
         /// <returns>The elapsed time between this DateTime object and now.</returns>
-        public static TimeSpan Elapsed(this DateTime datetime)
+        public static TimeSpan Elapsed(this DateTime dateTime)
         {
-            return DateTime.Now - datetime;
+            return DateTime.Now - dateTime;
         }
 
         /// <summary>
         /// Sets the time to "23:59:59:999".
         /// </summary>
-        /// <param name="datetime">The DateTime object to act on.</param>
+        /// <param name="dateTime">The DateTime object to act on.</param>
         /// <returns>A new DateTime object with the time set to "23:59:59:999".</returns>
-        public static DateTime EndOfDay(this DateTime datetime)
+        public static DateTime EndOfDay(this DateTime dateTime)
         {
-            return new DateTime(datetime.Year, datetime.Month, datetime.Day).AddDays(1).Subtract(new TimeSpan(0, 0, 0, 0, 1));
+            return new DateTime(dateTime.Year, dateTime.Month, dateTime.Day).AddDays(1).Subtract(new TimeSpan(0, 0, 0, 0, 1));
+        }
+
+        /// <summary>
+        /// Returns a DateTime of the last day of the month with the time set to "23:59:59:999".
+        /// </summary>
+        /// <param name="dateTime">The DateTime object to act on.</param>
+        /// <returns>A DateTime of the last day of the month with the time set to "23:59:59:999".</returns>
+        public static DateTime EndOfMonth(this DateTime dateTime)
+        {
+            return new DateTime(dateTime.Year, dateTime.Month, 1).AddMonths(1).Subtract(new TimeSpan(0, 0, 0, 0, 1));
+        }
+
+        /// <summary>
+        /// Returns a DateTime of the last day of the week with the time set to "23:59:59:999".
+        /// </summary>
+        /// <param name="dateTime">The DateTime object to act on.</param>
+        /// <param name="startDayOfWeek">(Optional) the start day of week.</param>
+        /// <returns>A DateTime of the last day of the week with the time set to "23:59:59:999".</returns>
+        public static DateTime EndOfWeek(this DateTime dateTime, DayOfWeek startDayOfWeek = DayOfWeek.Sunday)
+        {
+            DateTime end = dateTime;
+            DayOfWeek endDayOfWeek = startDayOfWeek - 1;
+            if (endDayOfWeek < 0)
+            {
+                endDayOfWeek = DayOfWeek.Saturday;
+            }
+
+            if (end.DayOfWeek != endDayOfWeek)
+            {
+                if (endDayOfWeek < end.DayOfWeek)
+                {
+                    end = end.AddDays(7 - (end.DayOfWeek - endDayOfWeek));
+                }
+                else
+                {
+                    end = end.AddDays(endDayOfWeek - end.DayOfWeek);
+                }
+            }
+
+            return new DateTime(end.Year, end.Month, end.Day, 23, 59, 59, 999);
+        }
+
+        /// <summary>
+        /// Returns a DateTime of the last day of the year with the time set to "23:59:59:999".
+        /// </summary>
+        /// <param name="dateTime">The DateTime object to act on.</param>
+        /// <returns>A DateTime of the last day of the year with the time set to "23:59:59:999".</returns>
+        public static DateTime EndOfYear(this DateTime dateTime)
+        {
+            return new DateTime(dateTime.Year, 1, 1).AddYears(1).Subtract(new TimeSpan(0, 0, 0, 0, 1));
         }
 
     }
