1a21a5e78d3458322ac2dea506efc02fa9bf15d6
Lukas Eßmann <l.essmann@binarypanther.com>
2018-11-22 11:31:45 +0100

    Added ObjectExtensions
    - Added System.Object extension class
    - Added InvokeMethod extension methods

---
+++ BinaryTools.Extensions.Core/System.Object/ObjectExtensions.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace BinaryTools.Extensions.Core
+{
+    /// <summary>
+    /// A collection of helpful extension methods for the <see cref="Object"/> class.
+    /// </summary>
+    public static partial class ObjectExtensions
+    {
+
+        /// <summary>
+        /// Invokes a method regardless of its protection or visibility level.
+        /// </summary>
+        /// <param name="o">The object to act on.</param>
+        /// <param name="methodName">The name of method to invoke.</param>
+        /// <param name="args">Arguments to be passed to the invoked method.</param>
+        /// <returns>The return value of the invoked method or null.</returns>
+        public static object InvokeMethod(this object o, String methodName, params Object[] args)
+        {
+            Type[] methodParamTypes = args?.Select(p => p.GetType()).ToArray() ?? new Type[] { };
+            BindingFlags bindingFlags = BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static;
+            MethodInfo methodInfo = o.GetType().GetMethod(methodName, bindingFlags, Type.DefaultBinder, methodParamTypes, null);
+            try
+            {
+                if (methodName != null)
+                {
+                    return methodInfo.Invoke(o, args);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex.InnerException;
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Invokes a method regardless of its protection or visibility level.
+        /// </summary>
+        /// <typeparam name="T">The return type of the invoked method.</typeparam>
+        /// <param name="o">The object to act on.</param>
+        /// <param name="methodName">The name of method to invoke.</param>
+        /// <param name="args">Arguments to be passed to the invoked method.</param>
+        /// <returns>The return value of the invoked method or the default of T.</returns>
+        public static T InvokeMethod<T>(this object o, String methodName, params Object[] args)
+        {
+            Type[] methodParamTypes = args?.Select(p => p.GetType()).ToArray() ?? new Type[] { };
+            BindingFlags bindingFlags = BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static;
+            MethodInfo methodInfo = o.GetType().GetMethod(methodName, bindingFlags, Type.DefaultBinder, methodParamTypes, null);
+            try
+            {
+                if (methodName != null)
+                {
+                    return (T)methodInfo.Invoke(o, args);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex.InnerException;
+            }
+            return default(T);
+        }
+    }
+}
