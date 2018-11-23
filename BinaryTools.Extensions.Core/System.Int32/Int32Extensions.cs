f08291a95caff9d2665a182364ebf143dd5e73e4
Lukas Eßmann <l.essmann@binarypanther.com>
2018-11-23 07:40:54 +0100

    Added ArabicToRoman and RomanToArabic conversion
    - Added ToRoman extension method to the Int32Extensions class
    - Added ToArabic extension method to the StringExtensions class

--- BinaryTools.Extensions.Core/System.Int32/Int32Extensions.cs
+++ BinaryTools.Extensions.Core/System.Int32/Int32Extensions.cs
@@ -16,5 +16,52 @@ namespace BinaryTools.Extensions.Core
         {
             return String.Format("0x{0:X}", value);
         }
+
+        /// <summary>
+        /// Converts an arabic number to an roman string.
+        /// </summary>
+        /// <param name="value">The Int32 to act on.</param>
+        /// <returns>Returns the converted value.</returns>
+        public static string ToRoman(this Int32 value)
+        {
+            string[] ThouLetters = { "", "M", "MM", "MMM" };
+            string[] HundLetters = { "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM" };
+            string[] TensLetters = { "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC" };
+            string[] OnesLetters = { "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
+
+            // See if it's >= 4000.
+            if (value >= 4000)
+            {
+                // Use parentheses.
+                int thou = value / 1000;
+                value %= 1000;
+                return "(" + ToRoman(thou) + ")" + ToRoman(value);
+            }
+
+            // Otherwise process the letters.
+            string result = "";
+
+            // Pull out thousands.
+            int num;
+            num = value / 1000;
+            result += ThouLetters[num];
+            value %= 1000;
+
+            // Handle hundreds.
+            num = value / 100;
+            result += HundLetters[num];
+            value %= 100;
+
+            // Handle tens.
+            num = value / 10;
+            result += TensLetters[num];
+            value %= 10;
+
+            // Handle ones.
+            result += OnesLetters[value];
+
+            return result;
+        }
+
     }
 }
--- BinaryTools.Extensions.Core/System.String/StringExtensions.cs
+++ BinaryTools.Extensions.Core/System.String/StringExtensions.cs
@@ -22,7 +22,7 @@ namespace BinaryTools.Extensions.Core
         /// <param name="str">The string that contains the placeholders.</param>
         /// <param name="args">The placeholders and actual values to replace them with.</param>
         /// <returns></returns>
-        public static string Format(this string str, params Expression<Func<string, object>>[] args)
+        public static String Format(this String str, params Expression<Func<String, Object>>[] args)
         {
             Dictionary<string, object> parameters = args.ToDictionary(e => string.Format("{{{0}}}", e.Parameters[0].Name), e => e.Compile()(e.Parameters[0].Name));
 
@@ -39,7 +39,7 @@ namespace BinaryTools.Extensions.Core
         /// </summary>
         /// <param name="value">The hexadecimal string.</param>
         /// <returns>A 32-bit signed integer equivalent to the hexadecimal string.</returns>
-        public static Int32 FromHex(this string value)
+        public static Int32 FromHex(this String value)
         {
             // strip the leading 0x
             if (value.StartsWith("0x", StringComparison.OrdinalIgnoreCase))
@@ -54,7 +54,7 @@ namespace BinaryTools.Extensions.Core
         /// </summary>
         /// <param name="str">The string to act on.</param>
         /// <returns>The string compressed into a GZip byte array.</returns>
-        public static byte[] CompressGZip(this string str)
+        public static Byte[] CompressGZip(this String str)
         {
             byte[] stringAsBytes = Encoding.Default.GetBytes(str);
             using (var memoryStream = new MemoryStream())
@@ -74,7 +74,7 @@ namespace BinaryTools.Extensions.Core
         /// <param name="str">The string to act on.</param>
         /// <param name="encoding">The <see cref="Encoding"/> to use.</param>
         /// <returns>The string compressed into a GZip byte array.</returns>
-        public static byte[] CompressGZip(this string str, Encoding encoding)
+        public static Byte[] CompressGZip(this String str, Encoding encoding)
         {
             byte[] stringAsBytes = encoding.GetBytes(str);
             using (var memoryStream = new MemoryStream())
@@ -86,6 +86,60 @@ namespace BinaryTools.Extensions.Core
                     return (memoryStream.ToArray());
                 }
             }
+        }
+
+        /// <summary>
+        /// Convert roman numerals to an integer.
+        /// </summary>
+        /// <param name="str">The string to act on.</param>
+        /// <returns>Returns the roman string as integer.</returns>
+        public static Int32 ToArabic(this String str)
+        {
+            // Initialize the letter map.
+            Dictionary<char, int> CharValues = new Dictionary<char, int>();
+            CharValues.Add('I', 1);
+            CharValues.Add('V', 5);
+            CharValues.Add('X', 10);
+            CharValues.Add('L', 50);
+            CharValues.Add('C', 100);
+            CharValues.Add('D', 500);
+            CharValues.Add('M', 1000);
+
+            if (str.Length == 0) return 0;
+            str = str.ToUpper();
+
+            // See if the number begins with (.
+            if (str[0] == '(')
+            {
+                // Find the closing parenthesis.
+                int pos = str.LastIndexOf(')');
+
+                // Get the value inside the parentheses.
+                string part1 = str.Substring(1, pos - 1);
+                string part2 = str.Substring(pos + 1);
+                return 1000 * ToArabic(part1) + ToArabic(part2);
+            }
+
+            // The number doesn't begin with (.
+            // Convert the letters' values.
+            int total = 0;
+            int last_value = 0;
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                int new_value = CharValues[str[i]];
+
+                // See if we should add or subtract.
+                if (new_value < last_value)
+                    total -= new_value;
+                else
+                {
+                    total += new_value;
+                    last_value = new_value;
+                }
+            }
+
+            // Return the result.
+            return total;
         }
 
     }
