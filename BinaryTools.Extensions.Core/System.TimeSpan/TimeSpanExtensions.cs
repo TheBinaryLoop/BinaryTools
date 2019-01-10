979ae2240dd2ff555e8e583847992a983fde5701
Lukas Eßmann <l.essmann@binarypanther.com>
2019-01-10 10:10:29 +0100

    Added TimeSpanExtensions
    - Added System.TimeSpan extension class
    - Added GetYears extension methods
    - Added GetMonths extension methods

---
+++ BinaryTools.Extensions.Core/System.TimeSpan/TimeSpanExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BinaryTools.Extensions.Core
+{
+    /// <summary>
+    /// A collection of helpful extension methods for the <see cref="TimeSpan"/> class.
+    /// </summary>
+    public static partial class TimeSpanExtensions
+    {
+
+        public static Int32 GetYears(this TimeSpan timespan)
+        {
+            return (Int32)(timespan.Days / 365.2425);
+        }
+        public static Int32 GetMonths(this TimeSpan timespan)
+        {
+            return (Int32)(timespan.Days / 30.436875);
+        }
+
+    }
+}
